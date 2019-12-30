a8f873b76aa0e7fdccabfa0619459c4f809143bd
mingli <mingli.yuan@gmail.com>
2019-12-30 16:31:33 +0800

    improve debug infomation

--- src/mainq.py
+++ src/mainq.py
@@ -26,8 +26,8 @@ from dqn import DQN, ReplayMemory, Transition, get_screen
 
 BATCH_SIZE = 128
 GAMMA = 0.999
-EPS_START = 0.9
-EPS_END = 0.05
+EPS_START = 0.8
+EPS_END = 0.01
 EPS_DECAY = 200
 TARGET_UPDATE = 10
 
