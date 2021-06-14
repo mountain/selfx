b0298cd6ef77b38a22b4b2f7ec46f0d8ffb16445
mingli <mingli.yuan@gmail.com>
2021-06-14 12:16:40 +0800

    fix env

--- bin/env.py
+++ bin/env.py
@@ -59,9 +59,5 @@ Check virtual enviroment
 """
 
 if not os.path.exists(".py"):
-    if cur_version >= (3, 7, 5):
-        sys.argv = ['.py']
-        venv.cli_run(sys.argv)
-    else:
-        sys.argv = ['virtualenv', '.py']
-        venv.main()
+    sys.argv = ['.py']
+    venv.cli_run(sys.argv)
